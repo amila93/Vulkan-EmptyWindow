bb7698b2391f54cb2d4677d6ad9fe50aa64e50a6
Amila Abeygunasekara <auabey@gmail.com>
2021-06-08 23:10:35 +0530

    DrawingATriangle - [Challenge] Checking for supported extensions

--- 01_DrawingATriangle/HelloTriangleApplication.cpp
+++ 01_DrawingATriangle/HelloTriangleApplication.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <stdexcept>
 #include <vector>
+#include <string.h>
+#include <algorithm>
 
 void HelloTriangleApplication::run()
 {
@@ -48,6 +50,12 @@ void HelloTriangleApplication::createInstance()
     createInfo.sType = VK_STRUCTURE_TYPE_LOADER_INSTANCE_CREATE_INFO;
     createInfo.pApplicationInfo = &appInfo;
 
+    // Enumerating available extensions
+    uint32_t extensionsCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionsCount, nullptr);
+    std::vector<VkExtensionProperties> extensions(extensionsCount);
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionsCount, extensions.data());
+
     // Vulkan needs extensions to interface with the window system
     uint32_t glfwExtensionCount = 0;
     const char** glfwExtensions;
@@ -55,26 +63,25 @@ void HelloTriangleApplication::createInstance()
     // GLFW has a built-in function which returns required extensions
     glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
 
+    // Checking if any of the required extensions are missing
+    for (int i = 0; i < glfwExtensionCount; i++)
+    {
+        auto lambda = [&i, &glfwExtensions](VkExtensionProperties& extension) { return strcmp(extension.extensionName, glfwExtensions[i]) == 0; };
+
+        auto it = std::find_if(extensions.begin(), extensions.end(), lambda);
+
+        if (it == extensions.end())
+        {
+            std::cout << "Required extension not found: " << glfwExtensions[i] << std::endl;
+        }
+    }
+
     // Setting those global extensions to the Vulkan driver
     createInfo.enabledExtensionCount = glfwExtensionCount;
     createInfo.ppEnabledExtensionNames = glfwExtensions;
 
     // Global validation layers to enable
     createInfo.enabledLayerCount = 0;
-
-    // **** Printing available extensions ****
-    uint32_t extensionsCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionsCount, nullptr);
-
-    std::vector<VkExtensionProperties> extensions(extensionsCount);
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionsCount, extensions.data());
-
-    std::cout << "Available extensions:\n";
-    for (const auto& extension : extensions)
-    {
-        std::cout << '\t' << extension.extensionName << '\n';
-    }
-    // ***************************************
 
     // Creating the Vulkan instance
     if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
